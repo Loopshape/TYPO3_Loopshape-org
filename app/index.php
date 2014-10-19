d1b711e4b2f1fbd362c34e132de34143accd699c
Loopshape <webmaster@loopshape.com>
2014-10-19 13:48:57 +0200

    setup developer stack > main landingpage

--- app/index.php
+++ app/index.php
@@ -1,57 +1,3 @@
-<?php /*
-
-    class Module {
-
-    public function process() {
-
-    $context = 'DATENSTROM';
-
-    return $context;
-
-    }
-
-    }
-
-    $App = new Module();
-    ?>
-
-    <!DOCTYPE HTML>
-
-    <html>
-
-    <head>
-    <title>LOOPSHAPE &bull; Web Development Area</title>
-    <meta charset="UTF-8" />
-    </head>
-
-    <body>
-
-    <section id="apiModule">
-    <?php echo $App -> process();
-    ?>
-    </section>
-
-    <script>
-    var google = require('googleapis');
-    var OAuth2 = google.auth.OAuth2;
-
-    var oauth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
-
-    // generate a url that asks permissions for Google+ and Google Calendar scopes
-    var scopes = ['https://www.googleapis.com/auth/plus.me', 'https://www.googleapis.com/auth/calendar'];
-
-    var url = oauth2Client.generateAuthUrl({
-    access_type : 'offline', // 'online' (default) or 'offline' (gets refresh_token)
-    scope : scopes // If you only need one scope you can pass it as string
-    });
-    </script>
-
-    </body>
-
-    </html>
-*/
-?>
-
 <html>
     <head>
         <script>
@@ -73,7 +19,7 @@
             }
 
             function init() {
-                gapi.client.setApiKey('YOUR API KEY');
+                gapi.client.setApiKey('AIzaSyCOLtfC-yIboZD1bvTV3mPzSRfBH6M0kTo');
                 gapi.client.load('urlshortener', 'v1').then(makeRequest);
             }
         </script>
